714efad8a5070ff508b799ae01b2919f7acd3d32
Drakirus <pierrescalade@gmail.com>
2018-08-18 18:13:19 +0200

    :sparkles: Extract runFlutter into a interface

--- main.go
+++ main.go
@@ -9,11 +9,7 @@ package main
 #include <stdint.h>
 
 
-bool proxy_make_current(void *v);
-bool proxy_clear_current(void *v);
-bool proxy_present(void *v);
-uint32_t proxy_fbo_callback(void *v);
-bool proxy_make_resource_current(void *v);
+
 bool proxy_OnPlatformMessage(FlutterPlatformMessage *message,
                              void *window);
 
@@ -23,41 +19,6 @@ static char* converter(uint8_t *str, size_t size){
 }
 
 
-static FlutterResult runFlutter(uintptr_t window, FlutterEngine *engine, FlutterOpenGLRendererConfig * fop){
-
-	FlutterRendererConfig config = {};
-	config.type = kOpenGL;
-
-	config.open_gl.struct_size = sizeof(FlutterOpenGLRendererConfig);
-
-	config.open_gl.make_current = fop->make_current;
-	config.open_gl.clear_current = fop->clear_current;
-	config.open_gl.present = fop->present;
-	config.open_gl.fbo_callback = fop->fbo_callback;
-	config.open_gl.make_resource_current = fop->make_resource_current;
-
-	const char *args_arr[] = {
-		"",
-		"--dart-non-checked-mode",
-		NULL,
-	};
-
-	FlutterProjectArgs args = {};
-	args.struct_size = sizeof(FlutterProjectArgs);
-	args.main_path = "";
-	// args.assets_path = "/opt/flutter-desktop-embedding/example_flutter/build/flutter_assets";
-	// args.assets_path = "/home/drakirus/lab/flutter/smart_access/build/flutter_assets";
-	args.assets_path = "/home/drakirus/lab/go/src/flutter_desktop_go_embedding/example_flutter/build/flutter_assets";
-	args.packages_path = "";
-	args.icu_data_path = "/home/drakirus/lab/go/src/flutter_desktop_go_embedding/icudtl.dat";
-	args.command_line_argc = 2;
-	args.command_line_argv = args_arr;
-	args.platform_message_callback = (FlutterPlatformMessageCallback)proxy_OnPlatformMessage;
-
-
-	return FlutterEngineRun(FLUTTER_ENGINE_VERSION, &config, &args, (void*)window, engine);
-
-}
 
 
 */
@@ -184,27 +145,18 @@ func glfwWindowSizeCallback(w *glfw.Window, width int, height int) {
 	flutter.FlutterEngineSendWindowMetricsEvent(w.GetUserPointer(), event)
 }
 
-func runFlutter(window *glfw.Window) C.FlutterEngine {
+func runFlutter(window *glfw.Window) *flutter.FlutterOpenGL {
 
-	var engine C.FlutterEngine
+	flutterOGL := flutter.FlutterOpenGL{}
 
-	w := window.GLFWWindow()
+	result := flutterOGL.Run(window.GLFWWindow())
 
-	a := C.FlutterOpenGLRendererConfig{
-		make_current:          (C.BoolCallback)(C.proxy_make_current),
-		clear_current:         (C.BoolCallback)(C.proxy_clear_current),
-		present:               (C.BoolCallback)(C.proxy_present),
-		fbo_callback:          (C.UIntCallback)(C.proxy_fbo_callback),
-		make_resource_current: (C.BoolCallback)(C.proxy_make_resource_current),
-	}
-
-	result := C.runFlutter(C.ulong(w), &engine, &a)
-	if result != C.kSuccess && engine == nil {
+	if result != flutter.KSuccess {
 		window.Destroy()
 		panic("Couldn't launch the FlutterEngine")
 	}
 
-	window.SetUserPointer(unsafe.Pointer(engine))
+	window.SetUserPointer(unsafe.Pointer(flutterOGL.Engine))
 
 	width, height := window.GetSize()
 	glfwWindowSizeCallback(window, width, height)
@@ -213,37 +165,7 @@ func runFlutter(window *glfw.Window) C.FlutterEngine {
 	window.SetSizeCallback(glfwWindowSizeCallback)
 	window.SetMouseButtonCallback(glfwMouseButtonCallback)
 	window.SetCharCallback(glfwCharCallback)
-	return engine
-}
-
-//export proxy_make_current
-func proxy_make_current(v unsafe.Pointer) C.bool {
-	a := glfw.GoWindow(v)
-	a.MakeContextCurrent()
-	return C.bool(true)
-}
-
-//export proxy_clear_current
-func proxy_clear_current(v unsafe.Pointer) C.bool {
-	glfw.DetachCurrentContext()
-	return C.bool(true)
-}
-
-//export proxy_present
-func proxy_present(v unsafe.Pointer) C.bool {
-	a := glfw.GoWindow(v)
-	a.SwapBuffers()
-	return C.bool(true)
-}
-
-//export proxy_fbo_callback
-func proxy_fbo_callback(v unsafe.Pointer) C.uint32_t {
-	return C.uint32_t(0)
-}
-
-//export proxy_make_resource_current
-func proxy_make_resource_current(v unsafe.Pointer) C.bool {
-	return C.bool(false)
+	return &flutterOGL
 }
 
 var state struct {
@@ -323,7 +245,7 @@ func main() {
 
 	engine := runFlutter(window)
 
-	defer C.FlutterEngineShutdown(engine)
+	defer engine.Shutdown()
 
 	for !window.ShouldClose() {
 		// glfw.WaitEvents()
