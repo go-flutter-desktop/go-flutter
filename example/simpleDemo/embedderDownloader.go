53be955e77cf4421d6df1a9284c48c1f1446d015
Thibault Brocherieux <tbrocherieux@ippon.fr>
2018-11-25 13:01:21 +0100

    Added downloader tracker

--- example/simpleDemo/embedderDownloader.go
+++ example/simpleDemo/embedderDownloader.go
@@ -11,11 +11,68 @@ import (
     "os"
     "io/ioutil"
     "encoding/json"
+    "time"
+    "path"
+    "bytes"
+    "strconv"
 )
+
+// Function to prind download percent completion
+func PrintDownloadPercent(done chan int64, path string, total int64) {
+
+	var stop bool = false
+
+	for {
+		select {
+		case <-done:
+			stop = true
+		default:
+
+			file, err := os.Open(path)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fi, err := file.Stat()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			size := fi.Size()
+
+			if size == 0 {
+				size = 1
+			}
+
+			var percent float64 = float64(size) / float64(total) * 100
+            
+            // We use `\033[2K\r` to avoid carriage return, it will print above previous.
+            fmt.Printf("\033[2K\r %.0f %% / 100 %%", percent)
+		}
+
+		if stop {
+			break
+		}
+
+		time.Sleep(time.Second)
+	}
+}
 
 // Function to download file with given path and url.
 func DownloadFile(filepath string, url string) error {
 
+    file := path.Base(url)
+
+    // Print download url in case user needs it.
+	fmt.Printf("Downloading file from %s\n", url)
+
+	var path bytes.Buffer
+	path.WriteString(filepath)
+	path.WriteString("/")
+	path.WriteString(file)
+
+    start := time.Now()
+    
     // Create the file
     out, err := os.Create(filepath)
     if err != nil {
@@ -30,11 +87,24 @@ func DownloadFile(filepath string, url string) error {
     }
     defer resp.Body.Close()
 
+
+    size, err := strconv.Atoi(resp.Header.Get("Content-Length"))
+
+    done := make(chan int64)
+
+	go PrintDownloadPercent(done, filepath, int64(size))
+
+
     // Write the body to file
-    _, err = io.Copy(out, resp.Body)
+    n, err := io.Copy(out, resp.Body)
     if err != nil {
         return err
     }
+
+    done <- n
+
+	elapsed := time.Since(start)
+	log.Printf("\033[2K\rDownload completed in %s", elapsed)
 
     return nil
 }
@@ -45,20 +115,14 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
-    
-    // DEBUG
-    // fmt.Printf("The os is %s\n",platform)
+
 
     re := regexp.MustCompile(`Engine • revision (\w{10})`)
     shortRevision := re.FindStringSubmatch(string(out))[1]
 
     url := fmt.Sprintf("https://api.github.com/search/commits?q=%s", shortRevision)
-    
-    // DEBUG
-    //fmt.Printf("The url is %s\n",url)
 
     // This part is used to retrieve the full hash 
-
     req, err := http.NewRequest("GET", os.ExpandEnv(url), nil)
     if err != nil {
         // handle err
@@ -71,6 +135,7 @@ func main() {
         // handle err
         log.Fatal(err)
     }
+
     body, err := ioutil.ReadAll(resp.Body)
     defer resp.Body.Close()
     if err != nil {
