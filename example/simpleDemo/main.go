6b46aceac3b6b332a49f55e906dfd257baaed315
Thibault Brocherieux <tbrocherieux@ippon.fr>
2018-12-10 12:32:57 +0100

    Updated naming and removed useless comments

--- example/simpleDemo/main.go
+++ example/simpleDemo/main.go
@@ -52,14 +52,9 @@ func main() {
 
 	options := []gutter.Option{
 		gutter.ProjectAssetPath(dir + "/flutter_project/demo/build/flutter_assets"),
-		/* Depending on your architecture you need to change the enginer
-		 * Mac OS X : flutter/bin/cache/artifacts/engine/darwin-x64/icudtl.dat
-		 * Linux    : flutter/bin/cache/artifacts/engine/linux-x64/icudtl.dat
-		 * Windows  : flutter/bin/cache/artifacts/engine/windows-x64/icudtl.dat
-		 */
-		gutter.OptionICUDataPath(dir + "/icudtl.dat"),
-		gutter.OptionWindowInitializer(setIcon),
-		gutter.OptionWindowDimension(initialApplicationWidth, initialApplicationHeight),
+		/* This path should not be changed. icudtl.dat is handled by engineDownloader.go */
+		gutter.ApplicationICUDataPath(dir + "/icudtl.dat"),
+		gutter.ApplicationWindowDimension(initialApplicationWidth, initialApplicationHeight),
 		gutter.OptionWindowInitializer(setIcon),
 		gutter.OptionPixelRatio(1.2),
 		gutter.OptionVMArguments([]string{"--dart-non-checked-mode", "--observatory-port=50300"}),
