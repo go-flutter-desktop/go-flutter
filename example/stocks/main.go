8b5b62d154d281a231a9ba9aaf0805fd7d7cefb9
Tycho Luyben <tycho@e-lab.nl>
2019-03-05 13:26:09 +0100

    updated stocks demo so it work with flutter 1.2.1

--- example/stocks/main.go
+++ example/stocks/main.go
@@ -5,35 +5,18 @@ import (
 	_ "image/png"
 	"log"
 	"os"
+	"path"
+	"runtime"
 
-	gutter "github.com/go-flutter-desktop/go-flutter"
+	"github.com/go-flutter-desktop/go-flutter"
 
 	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
-func main() {
-	var (
-		err error
-	)
-
-	options := []gutter.Option{
-		gutter.ProjectAssetPath("flutter_project/stocks/build/flutter_assets"),
-		gutter.OptionICUDataPath("/opt/flutter/bin/cache/artifacts/engine/linux-x64/icudtl.dat"), // Linux (arch)
-		// gutter.OptionICUDataPath("./FlutterEmbedder.framework/Resources/icudtl.dat"),             // OSX
-		gutter.OptionWindowDimension(800, 600),
-		gutter.OptionWindowInitializer(setIcon),
-		gutter.OptionPixelRatio(1.2),
-		gutter.OptionVMArguments([]string{"--dart-non-checked-mode", "--observatory-port=50300"}),
-	}
-
-	if err = gutter.Run(options...); err != nil {
-		log.Fatalln(err)
-	}
-
-}
-
 func setIcon(window *glfw.Window) error {
-	imgFile, err := os.Open("assets/icon.png")
+	_, currentFilePath, _, _ := runtime.Caller(0)
+	dir := path.Dir(currentFilePath)
+	imgFile, err := os.Open(dir + "/assets/icon.png")
 	if err != nil {
 		return err
 	}
@@ -43,4 +26,39 @@ func setIcon(window *glfw.Window) error {
 	}
 	window.SetIcon([]image.Image{img})
 	return nil
+}
+
+func main() {
+
+	_, currentFilePath, _, _ := runtime.Caller(0)
+	dir := path.Dir(currentFilePath)
+
+	initialApplicationHeight := 600
+	initialApplicationWidth := 800
+
+	options := []flutter.Option{
+		flutter.ProjectAssetPath(dir + "/flutter_project/stocks/build/flutter_assets"),
+
+		// This path should not be changed. icudtl.dat is handled by engineDownloader.go
+		flutter.ApplicationICUDataPath(dir + "/icudtl.dat"),
+
+		flutter.ApplicationWindowDimension(initialApplicationWidth, initialApplicationHeight),
+		// gutter.OptionPixelRatio(1.2),
+		flutter.OptionWindowInitializer(setIcon),
+		flutter.OptionVMArguments([]string{
+			// "--disable-dart-asserts", // release mode flag
+			// "--disable-observatory",
+			"--observatory-port=50300",
+		}),
+
+
+		// Default keyboard is Qwerty, if you want to change it, you can check keyboard.go in gutter package.
+		// Otherwise you can create your own by usinng `KeyboardShortcuts` struct.
+		//flutter.OptionKeyboardLayout(flutter.KeyboardAzertyLayout),
+	}
+
+	if err := flutter.Run(options...); err != nil {
+		log.Fatalln(err)
+	}
+
 }
