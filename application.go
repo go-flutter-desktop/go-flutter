543d68d29a36fdcb7f5eccb3e2e083d22a581def
Drakirus <prr.champion@gmail.com>
2019-11-06 21:37:28 +0100

    Ensure openg.DeleteTextures runs on main thread

--- application.go
+++ application.go
@@ -64,6 +64,9 @@ func NewApplication(opt ...Option) *Application {
 // createResourceWindow creates an invisible GLFW window that shares the 'view'
 // window's resource context. This window is used to upload resources in the
 // background. Must be call after the 'view' window is created.
+//
+// Though optional, it is recommended that all embedders set this callback as
+// it will lead to better performance in texture handling.
 func createResourceWindow(window *glfw.Window) (*glfw.Window, error) {
 	glfw.WindowHint(glfw.Decorated, glfw.False)
 	glfw.WindowHint(glfw.Visible, glfw.False)
@@ -326,9 +329,12 @@ func (a *Application) Run() error {
 		eventLoop.WaitForEvents(func(duration float64) {
 			glfw.WaitEventsTimeout(duration)
 		})
+
+		// Execute tasks that MUST be run in the engine thread (!blocks rendering!)
 		glfwDebouceTasker.ExecuteTasks()
 		defaultPlatformPlugin.glfwTasker.ExecuteTasks()
 		messenger.engineTasker.ExecuteTasks()
+		texturer.engineTasker.ExecuteTasks()
 	}
 
 	fmt.Println("go-flutter: closing application")
